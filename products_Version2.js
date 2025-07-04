e1de5b8c13bead02d98bd4b881975f68676394d7
thecollectorsthread <209753052+thecollectorsthread@users.noreply.github.com>
2025-07-04 19:01:23 +0600

    Add files via upload

---
+++ products_Version2.js
@@ -0,0 +1,12 @@
+const products = [
+  {
+    name: "Home Jersey 2025",
+    price: "৳600",
+    image: "assets/jersey1.jpg"
+  },
+  {
+    name: "Away Jersey 2025",
+    price: "$54.99",
+    image: "assets/jersey2.jpg"
+  }
+];
